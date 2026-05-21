e69a21f1ffe7488630923880ae14deb66eeb6ddb
Nilesh Thakur <thakurnilesh2000@gmail.com>
2026-05-21 11:13:14 +0530

    merging for changes

--- BUILDIntro/server/routes/mailRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { sendPendingMails } from "../controllers/mailController.js";
-
-const router = express.Router();
-
-router.get("/send-mails", sendPendingMails);
-
-export default router;
